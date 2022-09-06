595c334b640f7d6c7111392eed1986f202e2a3e3
Professorpreneur <daviduche809@yahoo.com>
2022-09-06 22:28:31 +0200

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,4 +2,3 @@
 // Print a message depending of the number of arguments passed
 const messages = ['No argument', 'Argument found', 'Arguments found'];
 console.log(messages[Math.min(process.argv.length - 2, 2)]);
-
